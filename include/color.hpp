724d67b11c0b965bd38231a225fd05f24721f0fb
G4GA <damian.gaga02@gmail.com>
2024-06-21 11:33:39 -0600

    feat: add colouring and testing it

--- include/color.hpp
+++ include/color.hpp
@@ -8,9 +8,18 @@ namespace tColor {
     const std::string YELLOW = "\033[33m";
     const std::string BLUE = "\033[34m";
     const std::string ENDC = "\033[0m";
+    const std::string BOLD = "\033[1m";
 
-    std::string print_c(const std::string& t_str, const std::string &color) 
+    std::string color_o(const std::string& t_str, const std::string &color) 
     {
         return color + t_str + ENDC; 
     }
+
+   std::string printResult(bool assertion, const std::string &message)
+   {
+        std::string n_message = "=> Test [" + color_o(message, BOLD) + "] ";
+
+        return assertion ? n_message + color_o("OK", GREEN) + "\n":
+                       n_message + color_o("Failed", RED) + "\n";
+   }
 }
--- modules/polyCodec/tests/UT_decoded_audio.cpp
+++ modules/polyCodec/tests/UT_decoded_audio.cpp
@@ -1,5 +1,7 @@
+#include <cstdint>
 #include <iostream>
 #include <decoded_audio.hpp>
+#include <color.hpp>
 
 class UTDecodedAudio 
 {
@@ -10,7 +12,13 @@ class UTDecodedAudio
 
 void UTDecodedAudio::run()
 {
-    
+        std::cout << tColor::color_o("[DecodedAudio class tests]\n", tColor::BOLD);
+        uint32_t val = 10;
+        float fval = 10.5;
+        PCMVal test_pcmval_i(val);
+        PCMVal test_pcmval_f(fval);
+
+        std::cout << tColor::printResult(true, "Test");
 }
 
 int main()
@@ -18,6 +26,5 @@ int main()
     UTDecodedAudio u_tester;
 
     u_tester.run();
-
     return 0;
 }
